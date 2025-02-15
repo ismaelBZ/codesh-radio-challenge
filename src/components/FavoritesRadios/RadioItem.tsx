6b6e0a1c6c54e37198a4e29680917484e3ab9a4e
ismael_BZ <ismaelbetzoletti@gmail.com>
2025-02-15 17:49:15 -0300

    favorites radios edit station done

--- src/components/FavoritesRadios/RadioItem.tsx
+++ src/components/FavoritesRadios/RadioItem.tsx
@@ -1,10 +1,9 @@
-import { ChangeEvent, useState } from "react";
+import { useEffect, useState } from "react";
 import playIcon from "./../../assets/icons/play-circle-icon.svg";
 import stopIcon from "./../../assets/icons/pause-circle-icon.svg";
 import deleteIcon from "./../../assets/icons/delete-icon.svg";
 import editIcon from "./../../assets/icons/edit-icon.svg";
 import { RadioItemT } from "../../@types/RadioItem";
-import { Station } from "../../@types/Station";
 
 const RadioItem = ( { name, stationuuid, url_resolved, country, tags, favoritesRadios, setCurrentStation, setFavoritesRadios } : RadioItemT) => {
     const [isNotPlaying, setIsNotPlaying] = useState(false);
@@ -22,22 +21,33 @@ const RadioItem = ( { name, stationuuid, url_resolved, country, tags, favoritesR
         })
     }
 
-    // const handleNameChange = (e: ChangeEvent<HTMLInputElement>, stationuuid: string) => {
-    //     const index = favoritesRadios?.findIndex(station => station.stationuuid === stationuuid);
-    //     if (index && e) {
-    //         setFavoritesRadios((prev) => {
-    //             const RenamedRadio = {
-    //                 ...prev![index],
-    //                 name: e.target.value
-    //             }
-    //             const newList = prev?.slice(0)
-    //             newList?.splice(index, 1, RenamedRadio);
-    //             return newList
-    //     })
-    //     }
-    // }
+    const handleFavoritesRadios = () => {
+        setFavoritesRadios((prev) => {
+            const radioIndex = prev!.findIndex((radio) => radio.stationuuid === stationuuid )
+            const updatedFavorites = prev!.slice(0);
+            // const newName = radioName.match(new RegExp(/^ *$/)) ? prev![radioIndex].name : radioName
+            updatedFavorites[radioIndex].name = radioName;
+            updatedFavorites[radioIndex].country = radioCountry;
+            return updatedFavorites;
+        });
+        (document.activeElement as HTMLInputElement).blur();
+        setIsEditingRadio(false);
+    }
 
-    
+    const handleInputBlur = () => {
+        const nameInput = document.getElementById(`${stationuuid}name`);
+        const countryInput = document.getElementById(`${stationuuid}country`);
+        setTimeout(() => {
+            // If active element in none of editing inputs, apply changes and close edition
+            if (document.activeElement === nameInput || document.activeElement === countryInput) {
+                return
+            } else {
+                (document.activeElement as HTMLInputElement).blur();
+                handleFavoritesRadios();
+            }
+        }, 80) // Need to run inside a setTimeout because onBlur doesn't have any active element
+    }
+
     const handleDeleteRadio = (stationuuid: string) => {
         setFavoritesRadios((prev) => {
             if (prev) {
@@ -48,9 +58,10 @@ const RadioItem = ( { name, stationuuid, url_resolved, country, tags, favoritesR
             }
         });
     }
-    
+
     return (
         <div className="-mx-[6px] py-[15px] px-[12px] relative">
+            
             {/* Background blur */}
             <div className="absolute top-[0] left-0 w-[100%] h-[100%] 
                 bg-radio rounded-sm blur-[1px]
@@ -58,34 +69,47 @@ const RadioItem = ( { name, stationuuid, url_resolved, country, tags, favoritesR
             ></div>
 
             <div className="relative z-10 flex justify-between items-center">
-                {/* Button & Info */}
-                <div className="flex gap-[10px] items-center">
-                    {/* Play Pause */}
+                <div className="flex gap-[10px] grow items-center">
+
+                    {/* Play Pause Button*/}
                     <button onClick={handleStation}>
                         <img src={isNotPlaying ? playIcon : stopIcon} alt={isNotPlaying ? "Play" : "Stop"} />
                     </button>
 
+                    {/* Audio */}
                     <audio src={url_resolved} />
 
                     {/* Radio Info */}
-                    <div>
+                    <div className="flex flex-col grow gap-1">
                         { isEditingRadio ? 
-                            <form className="flex flex-col">
+                            <> {/* Is used onChange to set the data for not rerender all radios on type*/}
                                 <input
+                                    id={`${stationuuid}name`}
                                     placeholder={radioName}
-                                    onChange={(e) => setRadioName(e.target.value)}
+                                    onChange={(e) => setRadioName(e.target.value)} 
+                                    onKeyDown={(e) => {if (e.key === "Enter") handleFavoritesRadios()}}
+                                    onBlur={handleInputBlur} 
+                                    autoFocus
                                     value={radioName}
-                                    className="font-semibold text-2xl placeholder:text-black"
+                                    className="font-semibold text-2xl placeholder:text-black rounded-sm bg-[rgba(123,241,168,0.5)]
+                                    shadow-[inset_0px_0px_5px_rgba(0,0,0,0.3),0px_0px_2px_rgba(0,0,0,0.5)] 
+                                    focus-visible:outline-0 focus-visible:border
+                                    "
                                     type="text" 
                                 />
                                 <input
+                                    id={`${stationuuid}country`}
                                     placeholder={radioCountry} 
                                     onChange={(e) => setRadioCountry(e.target.value)}
+                                    onKeyDown={(e) => {if (e.key === "Enter") handleFavoritesRadios()}}
+                                    onBlur={handleInputBlur}  
                                     value={radioCountry} 
-                                    className="hidden capitalize lg:block placeholder:text-black" 
+                                    className="hidden capitalize lg:block placeholder:text-black rounded-sm bg-[rgba(225,113,0,0.5)] 
+                                    shadow-[inset_0px_0px_5px_rgba(0,0,0,0.3),0px_0px_2px_rgba(0,0,0,0.5)]
+                                    focus-visible:outline-0 focus-visible:border" 
                                     type="text" 
                                 />
-                            </form>
+                            </>
                         :
                             <>
                                 <h2 className="font-semibold text-2xl">{radioName}</h2>
@@ -97,12 +121,16 @@ const RadioItem = ( { name, stationuuid, url_resolved, country, tags, favoritesR
 
                 {/* Edit & Delete Radio */}
                 <div>
-                    <button onClick={() => setIsEditingRadio(!isEditingRadio)} 
-                        className="hidden lg:inline-block lg:mr-[26px]"
+                    {/* edit */}
+                    <button 
+                        onClick={() => setIsEditingRadio(!isEditingRadio)}
+                        className="hidden lg:inline-block lg:px-[15px]"
                     >
                         <img src={editIcon} alt="Edit radio"/>
                     </button>
-                    <button className="mr-1" onClick={() => handleDeleteRadio(stationuuid)}>
+                    
+                    {/* delete */}
+                    <button className="mr-1 lg:ml-[5px] lg:p-[10px]" onClick={() => handleDeleteRadio(stationuuid)}>
                         <img src={deleteIcon} alt="Delete radio"/>
                     </button>
                 </div>
